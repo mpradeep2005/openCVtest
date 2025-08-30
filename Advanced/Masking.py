c67961eccd359ed007c4c22502fcff431955fc9b
Pradeep <pradeep14.3.2005@gmail.com>
2025-08-30 07:12:17 +0530

    Masking

---
+++ Advanced/Masking.py
@@ -0,0 +1,17 @@
+import cv2 as cv
+import numpy as np
+
+img=cv.imread("E:/openCVtest/res/AOT.jpeg")
+cv.imshow("noraml",img)
+# The blank(mask) size should be same as the normal
+blank=np.zeros(img.shape[:2],dtype="uint8")
+cv.imshow("balnk",blank)
+
+Rectangle=cv.rectangle(blank.copy(),(100,100),(400,400),250,-1)
+circle=cv.circle(blank.copy(),(350,200),100,250,-1)
+mask_img=cv.bitwise_and(Rectangle,circle)
+""" Mask the img,img with the mask"""
+masked=cv.bitwise_and(img,img,mask=mask_img)
+cv.imshow("masked",masked)
+
+cv.waitKey(0)
